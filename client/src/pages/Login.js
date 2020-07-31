e41b7a27ae81146aef003509f5dc3dfcd657f628
Drew Swinicki <dswinicki93@gmail.com>
2020-07-30 21:21:09 -0400

    added logic for functions for login and signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,13 +6,50 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
-import { Box, Button as GrommetButton, FormField, TextInput,Grommet } from 'grommet';
+import {
+  Box,
+  Button as GrommetButton,
+  FormField,
+  TextInput,
+  Grommet,
+} from "grommet";
 
 // passport logic for user sign up and log in
 // DB logic
 
-function Login() {
+// function to perform post route to api/signupuser
 
+function signUpUser(email, password) {
+  $.post("/api/signupuser", {
+    email: email,
+    password: password,
+  })
+
+    .then(function (data) {
+      console.log(data);
+      window.location.replace("/login");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+//function to perform post route  to api/loginuser
+function loginUser(email, password) {
+  $.post("/api/loginuser", {
+    email: email,
+    password: password,
+  })
+    .then(function () {
+      window.location.replace("/app");
+      // If there's an error, log the error
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function Login() {
   return (
     <Grommet>
       <Box>
@@ -22,20 +59,18 @@ function Login() {
             <TextInput placeholder="username" />
             <TextInput placeholder="password" />
           </FormField>
-          <GrommetButton color="primary" label='Login' />
+          <GrommetButton color="primary" label="Login" />
         </Box>
         <Box>
           <FormField label="Sign Up">
             <TextInput placeholder="username" />
             <TextInput placeholder="password" />
           </FormField>
-          <GrommetButton color="primary" label='Sign Up' />
+          <GrommetButton color="primary" label="Sign Up" />
         </Box>
       </Box>
     </Grommet>
-
   );
 }
-
 
 export default Login;
