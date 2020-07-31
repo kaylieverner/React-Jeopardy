2fd32839b6c7799ba16bf4a3cb8858f6d9479ed2
Kaylie V <k.verner37@gmail.com>
2020-07-30 20:02:30 -0400

    creating question card and category card components to populate on game board

--- client/src/components/QuestionCard/QuestionCard.js
+++ client/src/components/QuestionCard/QuestionCard.js
@@ -2,17 +2,13 @@ import React from "react";
 
 function QuestionCard() {
   return (
-<div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Category</h5>
-          
-          <p className="card-text">Question</p>
-          <a href="#" className="card-link">Show Answer</a>
-          <a href="#" className="card-link">Next Question</a>
-        </div>
+  <div className="col">
+    <div className="card">
+      <div className="card-body text-center">
+        <button type="button" class="btn btn-link">200</button>
       </div>
-</div>
+    </div>
+  </div>
   );
 }
 
