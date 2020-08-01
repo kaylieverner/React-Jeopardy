82912befab723401474c19fc0b638527438b257d
inafis <kbrown87@its.jnj.com>
2020-08-01 12:14:37 -0400

    initial implementation

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -6,59 +6,96 @@ import API from "../../utils/API";
 function Board() {
 const [categories, setCategories] = useState([]);
 const [questions, setQuestions] = useState([]);
+const [oneHundredQuestions, setOneHundredQuestions] = useState([]);
+const [twoHundredQuestions, setTwoHundredQuestions] = useState([]);
+const [threeHundredQuestions, setThreeHundredQuestions] = useState([]);
+const [fourHundredQuestions, setFourHundredQuestions] = useState([]);
+const [fiveHundredQuestions, setFiveHundredQuestions] = useState([]);
+
 
 useEffect(() => {
     loadCategories()
+    
   }, [])
 
   useEffect(() => {
-      console.log(categories)
       loadQuestions()
   }, [categories])
 
   useEffect(() => {
-    console.log(questions)
+    if(questions.length > 5){
+        categories.forEach(()=>{
+            setOneHundredQuestions(createScoreCards(0));
+            setTwoHundredQuestions(createScoreCards(1));
+            setThreeHundredQuestions(createScoreCards(2));
+            setFourHundredQuestions(createScoreCards(3));
+            setFiveHundredQuestions(createScoreCards(4));
+        })
+        console.log(questions)
+    }
 }, [questions])
 
   function loadCategories() {
     API.getCategories()
     .then(res => {
         setCategories(res.data)
-        console.log(res.data)
     })
     .catch(err => console.log(err));
   };
 
   function loadQuestions() {
-    categories.forEach((category) => {
+    categories.forEach((category, index) => {
+        console.log(`${category.id} ${index} ${typeof index}`);
         API.getQuestions(category.id)
         .then(res => {
-        setQuestions([...questions, questions.push({id:category.id, questions: res.data.clues})])
+            setQuestions([...questions, questions.push({id:category.id, questions: res.data.clues})]);
         })
     })
+  };
+
+  function createScoreCards(questionScoreIndex){
+    let tempArray = [];
+    questions.forEach((categoryQuestion)=>{
+        if(categoryQuestion.questions){
+            tempArray.push(categoryQuestion.questions[questionScoreIndex])
+        }
+    })
+    return tempArray
   }
+  
+  
 
   return (
     <div className="boardContainer">
         <div className="row categoryRow">
-  {categories.map((cat)=>{
-  return(
-  <CategoryCard title={cat.title}></CategoryCard>)})}
+            {categories.map((cat)=>{
+            return(
+            <CategoryCard title={cat.title}></CategoryCard>)})}
         </div>
         <div className="row 200Row">
-            <QuestionCard></QuestionCard>
+            {oneHundredQuestions.map((question)=>{
+                return <QuestionCard question={question} level='100'></QuestionCard>
+            })}
         </div>
-        <div className="row 400Row">
-            <QuestionCard></QuestionCard>
+        <div className="row 200Row">
+            {twoHundredQuestions.map((question)=>{
+                return <QuestionCard question={question} level='200'></QuestionCard>
+            })}
         </div>
-        <div className="row 600Row">
-            <QuestionCard></QuestionCard>
+        <div className="row 200Row">
+            {threeHundredQuestions.map((question)=>{
+                return <QuestionCard question={question} level='300'></QuestionCard>
+            })}
         </div>
-        <div className="row 800Row">
-            <QuestionCard></QuestionCard>
+        <div className="row 200Row">
+            {fourHundredQuestions.map((question)=>{
+                return <QuestionCard question={question} level='400'></QuestionCard>
+            })}
         </div>
-        <div className="row 1000Row">
-            <QuestionCard></QuestionCard>
+        <div className="row 200Row">
+            {fiveHundredQuestions.map((question)=>{
+                return <QuestionCard question={question} level='500'></QuestionCard>
+            })}
         </div>
     </div>
   );
--- client/src/components/QuestionCard/QuestionCard.js
+++ client/src/components/QuestionCard/QuestionCard.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { defaultProps } from "grommet";
 
-function QuestionCard() {
+function QuestionCard(props) {
   return (
   <div className="col">
     <div className="card">
       <div className="card-body text-center">
-        <button type="button" class="btn btn-link">200</button>
+        <button type="button" class="btn btn-link">{props.level}</button>
       </div>
     </div>
   </div>
