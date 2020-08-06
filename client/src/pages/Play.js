e36d516392830da84834928cc5508bb67aa9a073
Kaylie V <k.verner37@gmail.com>
2020-08-06 19:51:55 -0400

    adding functionality to define number of players playing

--- client/src/pages/Play.js
+++ client/src/pages/Play.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {Box} from 'grommet';
 import Container from "../components/Container/Container";
 import PlayerScore from "../components/PlayerScore/PlayerScore";
 import Board from "../components/Board/Board";
+import { Button } from 'react-bootstrap';
 
 
 // set up game board 
@@ -12,15 +13,30 @@ import Board from "../components/Board/Board";
 
 
 function Play() {
-  //
+const [numOfPlayers, setNumOfPlayers] = useState(0);
+const playerBtn = document.getElementById("playerBtn");
+
+function players() {
+  console.log(playerBtn.getAttribute("id"))
+}
+  
+
   return (
     <div className="container mt-4">
       <div className="row">
         <div className="col">
           <Container>
-            <div className="boardDiv">
-              <Board/>
-            </div>
+            <h3>Choose the Number of Players</h3>
+            <Button color="primary" className="onePlayers" id="btn1" onClick={players()}>1</Button>
+            <Button color="primary" className="twoPlayers" id="playerBtn" onClick={players()}>2</Button>
+            <Button color="primary" className="threePlayers" id="playerBtn" onClick={players()}>3</Button>
+            <Button color="primary" className="fourPlayers" id="playerBtn" onClick={players()}>4</Button>
+          </Container>
+          <Container>
+          <h1 className="mb-2">Scores</h1>
+            <Box direction="row-responsive" gap="small">
+              
+            </Box>
           </Container>
         </div>
       </div>
@@ -28,13 +44,9 @@ function Play() {
       <div className="row">
         <div className="col mt-5">
           <Container>
-            <h1 className="mb-2">Scores</h1>
-            <Box direction="row-responsive" gap="small">
-              <PlayerScore></PlayerScore>
-              <PlayerScore></PlayerScore>
-              <PlayerScore></PlayerScore>
-              <PlayerScore></PlayerScore>
-            </Box>
+            <div className="boardDiv">
+              <Board/>
+            </div>
           </Container>
         </div>
       </div>
