36dd424f49f4a40cab8cd42e65e8e616bd1dc971
Drew Swinicki <dswinicki93@gmail.com>
2020-08-11 09:52:56 -0400

    accidently dleeted file

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Play from "./pages/Play";
