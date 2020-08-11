064b808ce5ac6cef631d658b36d1a3a4a0796f37
Drew Swinicki <dswinicki93@gmail.com>
2020-08-11 07:38:02 -0400

    trying to integrate passport inside react now

--- client/src/components/SignUpForm/signUpForm.js
+++ client/src/components/SignUpForm/signUpForm.js
@@ -1,52 +1,50 @@
 import React, { Component } from "react";
-import API from '../../utils/API'
+import axios from 'axios';
 
 
-class signUpForm extends Component {
-  // Setting the component's initial state
-  state = {
-    username: "",
-    password: "",
-  };
+class SignUpForm extends Component {
+	constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: '',
+			confirmPassword: '',
 
-  handleInputChange = (event) => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
+		}
+		this.handleFormSubmit = this.handleFormSubmit.bind(this)
+		this.handleInputChange = this.handleInputChange.bind(this)
+	}
+	handleInputChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+	handleFormSubmit(event) {
+		console.log('sign-up handleSubmit, username: ')
+		console.log(this.state.username)
+		event.preventDefault()
 
-    // Updating the input's state
-    this.setState({
-      [name]: value,
-    });
-  };
+		//request to server to add a new username/password
+		axios.post('/user/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('successful signup')
+					this.setState({ //redirect to login page
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('username already taken')
+				}
+			}).catch(error => {
+				console.log('signup error: ')
+				console.log(error)
 
-  handleFormSubmit = (event) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
-
-    //function to perform post route  to api/loginuser
-    function signUpUser(email, password) {
-      
-      API.signUpPost({
-        email: email,
-        password: password,
-      })
-        .then(function () {
-          window.location.replace("/login");
-          // If there's an error, log the error
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    }
-
-    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    this.setState({
-      username: "",
-      password: "",
-    });
-
-    signUpUser(this.state.username, this.state.password);
-  };
+			})
+	}
 
   render() {
     // Notice how each input has a `value`, `name`, and `onChange` prop
@@ -74,4 +72,4 @@ class signUpForm extends Component {
   }
 }
 
-export default signUpForm;
+export default SignUpForm;
