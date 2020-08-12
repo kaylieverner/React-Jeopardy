f4a2738e5202b06097c3d048f261041b42fd8c1e
Drew Swinicki <dswinicki93@gmail.com>
2020-08-11 20:35:06 -0400

    d

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var path =require('path');
-var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 //Gets homepage
 router.get('/', function(req,res,next){
@@ -10,33 +9,12 @@ router.get('/', function(req,res,next){
 
 
 //gets react app
-router.get('/app', isAuthenticated, function(req, res, next) {
+router.get('/app', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
  });
 
 
-
-// module.exports = function(app) {
-
-//   app.get("/", function(req, res) {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/members");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/signup.html"));
-//   });
-
-//   app.get("/login", function(req, res) {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/members");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/login.html"));
-//   });
-
  
-// };
-
 //  /* GET React App */
 // router.get(['/app', '/app/*'], function(req, res, next) {
 //  res.sendFile(path.join(__dirname, '../public', 'app.html'));
