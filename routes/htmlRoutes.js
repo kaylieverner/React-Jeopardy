23275612f614b469b738187bc8368ef9bf591e4a
Drew Swinicki <dswinicki93@gmail.com>
2020-08-11 20:30:18 -0400

    adding passport

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path =require('path');
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 //Gets homepage
 router.get('/', function(req,res,next){
@@ -9,10 +10,32 @@ router.get('/', function(req,res,next){
 
 
 //gets react app
-router.get('/app', function(req, res, next) {
+router.get('/app', isAuthenticated, function(req, res, next) {
   res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
  });
 
+
+
+// module.exports = function(app) {
+
+//   app.get("/", function(req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/signup.html"));
+//   });
+
+//   app.get("/login", function(req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+//   });
+
+ 
+// };
 
 //  /* GET React App */
 // router.get(['/app', '/app/*'], function(req, res, next) {
