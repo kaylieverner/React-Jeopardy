4e6f595aa41ba961fb7e9d1eac2b25b2c3dab249
Kaylie V <k.verner37@gmail.com>
2020-08-12 22:36:05 -0400

    comment out

--- client/src/App.js
+++ client/src/App.js
@@ -13,46 +13,46 @@ import { Grommet } from "grommet";
 const theme = { global: { colors: { doc: "#ff99cc" } } };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      loggedIn: false,
-      username: null,
-    };
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     loggedIn: false,
+  //     username: null,
+  //   };
 
-    this.getUser = this.getUser.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.updateUser = this.updateUser.bind(this);
-  }
+  //   this.getUser = this.getUser.bind(this);
+  //   this.componentDidMount = this.componentDidMount.bind(this);
+  //   this.updateUser = this.updateUser.bind(this);
+  // }
 
-  componentDidMount() {
-    this.getUser();
-  }
+  // componentDidMount() {
+  //   this.getUser();
+  // }
 
-  updateUser(userObject) {
-    this.setState(userObject);
-  }
+  // updateUser(userObject) {
+  //   this.setState(userObject);
+  // }
 
-  getUser() {
-    axios.get("/user").then((response) => {
-      console.log("Get user response: ");
-      console.log(response.data);
-      if (response.data.user) {
-        console.log("Get User: There is a user saved in the server session: ");
+  // getUser() {
+  //   axios.get("/user").then((response) => {
+  //     console.log("Get user response: ");
+  //     console.log(response.data);
+  //     if (response.data.user) {
+  //       console.log("Get User: There is a user saved in the server session: ");
 
-        this.setState({
-          loggedIn: true,
-          username: response.data.user.username,
-        });
-      } else {
-        console.log("Get user: no user");
-        this.setState({
-          loggedIn: false,
-          username: null,
-        });
-      }
-    });
-  }
+  //       this.setState({
+  //         loggedIn: true,
+  //         username: response.data.user.username,
+  //       });
+  //     } else {
+  //       console.log("Get user: no user");
+  //       this.setState({
+  //         loggedIn: false,
+  //         username: null,
+  //       });
+  //     }
+  //   });
+  // }
 
   render() {
     return (
